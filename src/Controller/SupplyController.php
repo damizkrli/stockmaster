b0f39b1308da950012fff25a05109d6570301989
Damien Carli <karli.damien@gmail.com>
2023-05-23 14:00:17 +0200

    UPDATE OutOfProductController and SupplyController

--- src/Controller/SupplyController.php
+++ src/Controller/SupplyController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Supply;
 use App\Form\SupplyType;
 use App\Repository\SupplyRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +15,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class SupplyController extends AbstractController
 {
     private SupplyRepository $supplyRepository;
+    private PaginatorInterface $paginator;
 
-    public function __construct(SupplyRepository $supplyRepository)
-    {
+    public function __construct(
+        SupplyRepository $supplyRepository,
+        PaginatorInterface $paginator
+    ) {
         $this->supplyRepository = $supplyRepository;
+        $this->paginator = $paginator;
     }
 
     #[Route('/', name: 'supply', methods: ['GET', 'POST'])]
@@ -27,15 +32,25 @@ class SupplyController extends AbstractController
         $form = $this->createForm(SupplyType::class, $supply);
         $form->handleRequest($request);
 
+        $suppliesQuery = $this->supplyRepository->findBy([], ['addedAt' => 'DESC']);
+
+        $supplies = $this->paginator->paginate(
+            $suppliesQuery,
+            $request->query->getInt('page', 1),
+            8
+        );
+
         if ($form->isSubmitted() && $form->isValid()) {
+            $newSupply = $form->getData();
             $this->supplyRepository->save($supply, true);
+            $this->addFlash('success', 'Votre fourniture à été ajouté.');
 
             return $this->redirectToRoute('supply', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('supply/index.html.twig', [
             'supplies' => $this->supplyRepository->findBy([], ['addedAt' => 'DESC']),
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 
@@ -47,6 +62,7 @@ class SupplyController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $supplyRepository->save($supply, true);
+            $this->addFlash('success', 'Votre fourniture a été modifié avec succès.');
 
             return $this->redirectToRoute('supply', [], Response::HTTP_SEE_OTHER);
         }
