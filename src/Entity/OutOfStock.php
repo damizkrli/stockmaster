8e255b0258c6f900d2f9befca310425b2cf41a50
Damien Carli <karli.damien@gmail.com>
2023-06-04 20:17:12 +0200

    FIX use php_cs_fixer

--- src/Entity/OutOfStock.php
+++ src/Entity/OutOfStock.php
@@ -4,7 +4,6 @@ namespace App\Entity;
 
 use App\Repository\OutOfStockRepository;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: OutOfStockRepository::class)]
