74228ed75a08f144b64b94c3536e6f2a599d442d
Damien Carli <karli.damien@gmail.com>
2023-06-04 22:24:30 +0200

    UPDATE tables product

--- src/DataFixtures/ProductsFixtures.php
+++ src/DataFixtures/ProductsFixtures.php
@@ -15,7 +15,7 @@ class ProductsFixtures extends Fixture
 
         for ($i = 0; $i < 500; ++$i) {
             $product = new Product();
-            $product->setName($faker->words(2, true))
+            $product->setName($faker->words(4, true))
                 ->setBrand($faker->words(1, true))
                 ->setReference($faker->bothify('???-###').'-'.$faker->bothify('???'))
                 ->setSerialNumber(mt_rand(100000000000, 999999999999))
