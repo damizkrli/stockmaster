01d454679539f49ae1903fc5ed72556a09eb8f46
Damien Carli <karli.damien@gmail.com>
2023-06-07 22:09:04 +0200

    UPDATE show product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -54,11 +54,11 @@ class ProductController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'product_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'product_show', methods: ['GET', 'POST'])]
     public function show(Product $product): Response
     {
         return $this->render('product/show.html.twig', [
-            'product' => $product,
+            'products' => $product,
         ]);
     }
 
