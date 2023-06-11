62b0507949715e3ec7514f4f50b1bb55edc90ae7
Damien Carli <karli.damien@gmail.com>
2023-06-11 22:20:37 +0200

    FIX product serialized

--- src/Controller/ProductSerializedController.php
+++ src/Controller/ProductSerializedController.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/serialized')]
 class ProductSerializedController extends AbstractController
 {
     private ProductSerializedRepository $productSerializedRepository;
